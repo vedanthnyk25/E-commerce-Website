9dd4aa974e4f1d9975e0ec4baaab204e93977f11
vedanthnyk25 <vedanthnayak25@gmail.com>
2025-01-16 23:47:27 +0530

    routes setup

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact' 
+import Cart from './pages/Cart'
+import Product from './pages/Product'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Collection from './pages/Collection'
+
+
+function App() {
+ 
+
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/collection' element={<Collection />} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
